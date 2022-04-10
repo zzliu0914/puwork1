dd42e514f9a8501ddb435984b8d6604192c95126
zzliu09 <tyliu0914@gmail.com>
2022-04-11 00:33:56 +0800

    第三次 Commit：正式實作

--- app/src/main/java/com/tyliu/puwork1/MainActivity.kt
+++ app/src/main/java/com/tyliu/puwork1/MainActivity.kt
@@ -9,50 +9,72 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity(),GestureDetector.OnGestureListener
-    ,GestureDetector.OnDoubleTapListener{
+    ,GestureDetector.OnDoubleTapListener,View.OnTouchListener{
 
     lateinit var gDetector: GestureDetector
+    var PictureNo:Int = 0  //目前顯示第幾張圖
+    var TotalPictures:Int = 6 //總共幾張圖片(假設僅顯示ph0-ph5)
+
+    fun ShowPicture() {
+        when (PictureNo) {
+            0 -> img.setImageResource(R.drawable.ph0)
+            1 -> img.setImageResource(R.drawable.ph1)
+            2 -> img.setImageResource(R.drawable.ph2)
+            3 -> img.setImageResource(R.drawable.ph3)
+            4 -> img.setImageResource(R.drawable.ph4)
+            5 -> img.setImageResource(R.drawable.ph5)
+        }
+        txv.text = PictureNo.toString()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         gDetector = GestureDetector(this, this)
+        img.setOnTouchListener(this)
     }
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
+    /*override fun onTouchEvent(event: MotionEvent?): Boolean {
+        gDetector.onTouchEvent(event)
+        return true
+    }*/
+    override fun onTouch(p0: View?, event: MotionEvent?): Boolean {
         gDetector.onTouchEvent(event)
         return true
     }
 
 
-
     override fun onDown(p0: MotionEvent?): Boolean {
-        txv.text = "按下"
         return true
     }
 
     override fun onShowPress(p0: MotionEvent?) {
-        txv.text = "按下後無後續動作"
     }
 
     override fun onSingleTapUp(p0: MotionEvent?): Boolean {
-        txv.text = "短按"
+        PictureNo = 0
+        ShowPicture()
         return true
     }
 
     override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
-        txv.text = "拖曳\nx1y1: " +  e1?.getX().toString() + ", " + e1?.getY().toString() +
-                "\nx2y2: " + e2?.getX().toString() + ", " + e2?.getY().toString()
         return true
     }
 
     override fun onLongPress(p0: MotionEvent?) {
-        txv.text = "長按"
+        PictureNo = TotalPictures - 1
+        ShowPicture()
     }
 
     override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
-        txv.text = "快滑\nx1y1: " + e1?.getX().toString() + ", " + e1?.getY().toString() +
-                "\nx2y2: " + e2?.getX().toString() + ", " + e2?.getY().toString() +
-                "\nX軸Y軸速度:" + velocityX.toString() + ", " +  velocityY.toString()
+        if (e1!!.getX() < e2!!.getX()){  //向右快滑
+            PictureNo++
+            if (PictureNo == TotalPictures) {PictureNo = 0}
+        }
+        else{     //向左快滑
+            PictureNo--;
+            if (PictureNo < 0) {PictureNo = TotalPictures - 1 }
+        }
+        ShowPicture()
         return true
     }
 
@@ -65,9 +87,6 @@ class MainActivity : AppCompatActivity(),GestureDetector.OnGestureListener
     }
 
     override fun onDoubleTapEvent(p0: MotionEvent?): Boolean {
-        txv.text = "連續點兩下"
         return true
     }
-
-
 }
