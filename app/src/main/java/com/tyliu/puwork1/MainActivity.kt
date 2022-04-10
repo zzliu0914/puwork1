896e5a6291327f690211b9e68b78e35f0781f09f
zzliu09 <tyliu0914@gmail.com>
2022-04-11 00:37:14 +0800

    第四次 Commit：動態取得資源識別

--- app/src/main/java/com/tyliu/puwork1/MainActivity.kt
+++ app/src/main/java/com/tyliu/puwork1/MainActivity.kt
@@ -16,15 +16,19 @@ class MainActivity : AppCompatActivity(),GestureDetector.OnGestureListener
     var TotalPictures:Int = 6 //總共幾張圖片(假設僅顯示ph0-ph5)
 
     fun ShowPicture() {
-        when (PictureNo) {
+        /*when (PictureNo) {
             0 -> img.setImageResource(R.drawable.ph0)
             1 -> img.setImageResource(R.drawable.ph1)
             2 -> img.setImageResource(R.drawable.ph2)
             3 -> img.setImageResource(R.drawable.ph3)
             4 -> img.setImageResource(R.drawable.ph4)
             5 -> img.setImageResource(R.drawable.ph5)
-        }
+        }*/
         txv.text = PictureNo.toString()
+        var res:Int = getResources().getIdentifier("ph" + PictureNo.toString(),
+            "drawable", getPackageName())
+        img.setImageResource(res)
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -67,12 +71,13 @@ class MainActivity : AppCompatActivity(),GestureDetector.OnGestureListener
 
     override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
         if (e1!!.getX() < e2!!.getX()){  //向右快滑
-            PictureNo++
-            if (PictureNo == TotalPictures) {PictureNo = 0}
-        }
-        else{     //向左快滑
             PictureNo--;
             if (PictureNo < 0) {PictureNo = TotalPictures - 1 }
+        }
+        else{     //向左快滑
+
+            PictureNo++
+            if (PictureNo == TotalPictures) {PictureNo = 0}
         }
         ShowPicture()
         return true
